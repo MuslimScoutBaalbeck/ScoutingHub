27776a6c28b6c1e6d42ca3f1c584290d964e2041
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-02 13:54:34 +0300

    feat(auth): generate complete authentication routes

--- lib/core/router/app_router.gr.dart
+++ lib/core/router/app_router.gr.dart
@@ -9,76 +9,139 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i5;
-import 'package:scouting_hub/features/auth/presentation/pages/login_page.dart'
-    as _i2;
-import 'package:scouting_hub/features/auth/presentation/pages/register_page.dart'
-    as _i3;
-import 'package:scouting_hub/features/home/presentation/pages/home_page.dart'
+import 'package:auto_route/auto_route.dart' as _i8;
+import 'package:flutter/material.dart' as _i9;
+import 'package:scouting_hub/features/auth/presentation/pages/forgot_password_page.dart'
     as _i1;
-import 'package:scouting_hub/features/startup/presentation/pages/startup_page.dart'
+import 'package:scouting_hub/features/auth/presentation/pages/login_page.dart'
+    as _i3;
+import 'package:scouting_hub/features/auth/presentation/pages/register_page.dart'
     as _i4;
+import 'package:scouting_hub/features/auth/presentation/pages/reset_password_page.dart'
+    as _i5;
+import 'package:scouting_hub/features/auth/presentation/pages/welcome_page.dart'
+    as _i7;
+import 'package:scouting_hub/features/home/presentation/pages/home_page.dart'
+    as _i2;
+import 'package:scouting_hub/features/startup/presentation/pages/startup_page.dart'
+    as _i6;
 
-/// generated route for
-/// [_i1.HomePage]
-class HomeRoute extends _i5.PageRouteInfo<void> {
-  const HomeRoute({List<_i5.PageRouteInfo>? children})
-    : super(HomeRoute.name, initialChildren: children);
+class ForgotPasswordRoute extends _i8.PageRouteInfo<void> {
+  const ForgotPasswordRoute({List<_i8.PageRouteInfo>? children})
+      : super(ForgotPasswordRoute.name, initialChildren: children);
+
+  static const String name = 'ForgotPasswordRoute';
+
+  static _i8.PageInfo page = _i8.PageInfo(
+    name,
+    builder: (data) {
+      return _i8.WrappedRoute(child: const _i1.ForgotPasswordPage());
+    },
+  );
+}
+
+class HomeRoute extends _i8.PageRouteInfo<void> {
+  const HomeRoute({List<_i8.PageRouteInfo>? children})
+      : super(HomeRoute.name, initialChildren: children);
 
   static const String name = 'HomeRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i1.HomePage();
+      return const _i2.HomePage();
     },
   );
 }
 
-/// generated route for
-/// [_i2.LoginPage]
-class LoginRoute extends _i5.PageRouteInfo<void> {
-  const LoginRoute({List<_i5.PageRouteInfo>? children})
-    : super(LoginRoute.name, initialChildren: children);
+class LoginRoute extends _i8.PageRouteInfo<void> {
+  const LoginRoute({List<_i8.PageRouteInfo>? children})
+      : super(LoginRoute.name, initialChildren: children);
 
   static const String name = 'LoginRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return _i5.WrappedRoute(child: const _i2.LoginPage());
+      return _i8.WrappedRoute(child: const _i3.LoginPage());
     },
   );
 }
 
-/// generated route for
-/// [_i3.RegisterPage]
-class RegisterRoute extends _i5.PageRouteInfo<void> {
-  const RegisterRoute({List<_i5.PageRouteInfo>? children})
-    : super(RegisterRoute.name, initialChildren: children);
+class RegisterRoute extends _i8.PageRouteInfo<void> {
+  const RegisterRoute({List<_i8.PageRouteInfo>? children})
+      : super(RegisterRoute.name, initialChildren: children);
 
   static const String name = 'RegisterRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i3.RegisterPage();
+      return _i8.WrappedRoute(child: const _i4.RegisterPage());
     },
   );
 }
 
-/// generated route for
-/// [_i4.StartupPage]
-class StartupRoute extends _i5.PageRouteInfo<void> {
-  const StartupRoute({List<_i5.PageRouteInfo>? children})
-    : super(StartupRoute.name, initialChildren: children);
+class ResetPasswordRoute extends _i8.PageRouteInfo<ResetPasswordRouteArgs> {
+  ResetPasswordRoute({
+    required String email,
+    _i9.Key? key,
+    List<_i8.PageRouteInfo>? children,
+  }) : super(
+          ResetPasswordRoute.name,
+          args: ResetPasswordRouteArgs(email: email, key: key),
+          initialChildren: children,
+        );
+
+  static const String name = 'ResetPasswordRoute';
+
+  static _i8.PageInfo page = _i8.PageInfo(
+    name,
+    builder: (data) {
+      final args = data.argsAs<ResetPasswordRouteArgs>();
+      return _i8.WrappedRoute(
+        child: _i5.ResetPasswordPage(email: args.email, key: args.key),
+      );
+    },
+  );
+}
+
+class ResetPasswordRouteArgs {
+  const ResetPasswordRouteArgs({required this.email, this.key});
+
+  final String email;
+  final _i9.Key? key;
+
+  @override
+  String toString() {
+    return 'ResetPasswordRouteArgs{email: $email, key: $key}';
+  }
+}
+
+class StartupRoute extends _i8.PageRouteInfo<void> {
+  const StartupRoute({List<_i8.PageRouteInfo>? children})
+      : super(StartupRoute.name, initialChildren: children);
 
   static const String name = 'StartupRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i4.StartupPage();
+      return const _i6.StartupPage();
+    },
+  );
+}
+
+class WelcomeRoute extends _i8.PageRouteInfo<void> {
+  const WelcomeRoute({List<_i8.PageRouteInfo>? children})
+      : super(WelcomeRoute.name, initialChildren: children);
+
+  static const String name = 'WelcomeRoute';
+
+  static _i8.PageInfo page = _i8.PageInfo(
+    name,
+    builder: (data) {
+      return const _i7.WelcomePage();
     },
   );
 }
