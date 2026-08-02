628d056ca3ee79c2dcf4f7b1db41db5d133cfa3a
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-02 13:52:42 +0300

    feat(auth): add reset password use case

---
+++ lib/features/auth/domain/usecases/reset_password.dart
@@ -0,0 +1,23 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:injectable/injectable.dart';
+import 'package:scouting_hub/features/auth/domain/failures/auth_failure.dart';
+import 'package:scouting_hub/features/auth/domain/repositories/auth_repository.dart';
+
+@singleton
+final class ResetPassword {
+  const ResetPassword(this._repository);
+
+  final AuthRepository _repository;
+
+  Future<Either<AuthFailure, Unit>> call({
+    required String email,
+    required String code,
+    required String password,
+  }) {
+    return _repository.resetPassword(
+      email: email.trim().toLowerCase(),
+      code: code.trim(),
+      password: password,
+    );
+  }
+}
