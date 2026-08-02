99e2f7c6cdba00d4d409dd13b061a3c333e138dd
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-02 19:24:26 +0300

    feat(bootstrap): preload branding before runApp

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -10,6 +10,7 @@ import 'package:scouting_hub/core/database/connection/native.dart'
     as database_connection;
 import 'package:scouting_hub/core/di/injection.dart';
 import 'package:scouting_hub/core/i18n/translations.g.dart';
+import 'package:scouting_hub/core/ui/preload_branding_assets.dart';
 import 'package:scouting_hub/core/utils/observer/bloc_observer.dart';
 import 'package:scouting_hub/features/startup/application/application_start/application_start_cubit.dart';
 
@@ -40,7 +41,10 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
       applicationStartCubit.state.locale ??
       LocaleSettings.useDeviceLocaleSync().languageCode;
 
-  await LocaleSettings.setLocaleRaw(currentLocale);
+  await Future.wait([
+    LocaleSettings.setLocaleRaw(currentLocale),
+    preloadBrandingAssets(),
+  ]);
 
   runApp(
     TranslationProvider(
