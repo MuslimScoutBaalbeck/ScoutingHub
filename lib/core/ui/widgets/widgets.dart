5a80c36da86b7a5ff5251fe631141960b9f0a720
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-03 01:34:47 +0300

    refactor(ui): add atomic widget exports

---
+++ lib/core/ui/widgets/widgets.dart
@@ -0,0 +1,8 @@
+export 'atoms/app_back_button.dart';
+export 'atoms/app_button.dart';
+export 'atoms/app_gap.dart';
+export 'atoms/app_square_action.dart';
+export 'atoms/app_text.dart';
+export 'molecules/app_section_header.dart';
+export 'organisms/app_page_header.dart';
+export 'templates/app_page_template.dart';
