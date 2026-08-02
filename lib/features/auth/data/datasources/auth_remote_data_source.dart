aff37dc095a768ffc26ac947717c448e3eb05918
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-02 13:48:44 +0300

    feat(auth): add fake password recovery contracts

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -1,6 +1,5 @@
 import 'package:scouting_hub/features/auth/data/dtos/auth_response_dto.dart';
 
-
 abstract interface class AuthRemoteDataSource {
   Future<AuthResponseDto> login({
     required String email,
@@ -10,6 +9,14 @@ abstract interface class AuthRemoteDataSource {
   Future<AuthResponseDto> register({
     required String name,
     required String email,
+    required String password,
+  });
+
+  Future<void> forgotPassword({required String email});
+
+  Future<void> resetPassword({
+    required String email,
+    required String code,
     required String password,
   });
 
