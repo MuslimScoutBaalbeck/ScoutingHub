7290b48091b6659f3416ec1743fcc2a922d781f7
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-03 10:08:18 +0300

    feat(organizations): add scout position entity

---
+++ lib/features/organizations/domain/entities/scout_position.dart
@@ -0,0 +1,13 @@
+final class ScoutPosition {
+  const ScoutPosition({
+    required this.id,
+    required this.code,
+    required this.name,
+    required this.category,
+  });
+
+  final int id;
+  final String code;
+  final String name;
+  final String category;
+}
