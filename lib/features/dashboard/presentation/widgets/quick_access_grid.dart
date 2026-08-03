d3429d80b646eea126d6bfe361d8fcc92ae9cb93
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-03 10:39:14 +0300

    Make quick access cards square

--- lib/features/dashboard/presentation/widgets/quick_access_grid.dart
+++ lib/features/dashboard/presentation/widgets/quick_access_grid.dart
@@ -63,7 +63,7 @@ class QuickAccessGrid extends StatelessWidget {
         crossAxisCount: 3,
         mainAxisSpacing: AppSpacing.sm,
         crossAxisSpacing: AppSpacing.sm,
-        mainAxisExtent: 126,
+        childAspectRatio: 1,
       ),
       itemBuilder: (context, index) {
         final item = visibleItems[index];
