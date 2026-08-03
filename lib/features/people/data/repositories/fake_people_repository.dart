bfa7fc081d98a6e592064b580e349c7b7310d988
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-03 11:49:54 +0300

    feat(people): implement fake people repository

---
+++ lib/features/people/data/repositories/fake_people_repository.dart
@@ -0,0 +1,14 @@
+import 'package:injectable/injectable.dart';
+import 'package:scouting_hub/features/people/data/datasources/fake_people_data_source.dart';
+import 'package:scouting_hub/features/people/domain/entities/person.dart';
+import 'package:scouting_hub/features/people/domain/repositories/people_repository.dart';
+
+@LazySingleton(as: PeopleRepository)
+final class FakePeopleRepository implements PeopleRepository {
+  const FakePeopleRepository(this._dataSource);
+
+  final FakePeopleDataSource _dataSource;
+
+  @override
+  Future<List<Person>> getPeople() => _dataSource.loadPeople();
+}
