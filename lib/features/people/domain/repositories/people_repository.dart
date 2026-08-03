6a8419b3e8fbcbc0971316423e0b879d20a7f320
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-03 12:48:51 +0300

    feat(members): support saving members in repository

--- lib/features/people/domain/repositories/people_repository.dart
+++ lib/features/people/domain/repositories/people_repository.dart
@@ -2,4 +2,6 @@ import 'package:scouting_hub/features/people/domain/entities/person.dart';
 
 abstract interface class PeopleRepository {
   Future<List<Person>> getPeople();
+
+  Future<Person> savePerson(Person person);
 }
