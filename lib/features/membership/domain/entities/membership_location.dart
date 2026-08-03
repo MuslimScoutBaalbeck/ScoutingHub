c087aaf3445b3503884eb095056fab3f14fecdac
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-03 17:46:00 +0300

    Localize membership location entities

--- lib/features/membership/domain/entities/membership_location.dart
+++ lib/features/membership/domain/entities/membership_location.dart
@@ -1,52 +1,82 @@
+import 'package:scouting_hub/core/i18n/translations.g.dart';
+
+bool get _isArabic => LocaleSettings.currentLocale.languageCode == 'ar';
+
 final class MembershipGovernorate {
-  const MembershipGovernorate({required this.id, required this.name});
+  const MembershipGovernorate({
+    required this.id,
+    required this.nameEn,
+    required this.nameAr,
+  });
 
   final int id;
-  final String name;
+  final String nameEn;
+  final String nameAr;
+
+  String get name => _isArabic ? nameAr : nameEn;
 }
 
 final class MembershipDistrict {
   const MembershipDistrict({
     required this.id,
     required this.governorateId,
-    required this.name,
+    required this.nameEn,
+    required this.nameAr,
   });
 
   final int id;
   final int governorateId;
-  final String name;
+  final String nameEn;
+  final String nameAr;
+
+  String get name => _isArabic ? nameAr : nameEn;
 }
 
 final class MembershipCadaster {
   const MembershipCadaster({
     required this.id,
     required this.districtId,
-    required this.name,
+    required this.nameEn,
+    required this.nameAr,
     required this.commissionId,
-    required this.commissionName,
+    required this.commissionNameEn,
+    required this.commissionNameAr,
   });
 
   final int id;
   final int districtId;
-  final String name;
+  final String nameEn;
+  final String nameAr;
   final int commissionId;
-  final String commissionName;
+  final String commissionNameEn;
+  final String commissionNameAr;
+
+  String get name => _isArabic ? nameAr : nameEn;
+  String get commissionName =>
+      _isArabic ? commissionNameAr : commissionNameEn;
 }
 
 final class MembershipTroop {
   const MembershipTroop({
     required this.id,
     required this.cadasterId,
-    required this.name,
-    required this.address,
+    required this.nameEn,
+    required this.nameAr,
+    required this.addressEn,
+    required this.addressAr,
     required this.acceptingMembers,
   });
 
   final int id;
   final int cadasterId;
-  final String name;
-  final String address;
+  final String nameEn;
+  final String nameAr;
+  final String addressEn;
+  final String addressAr;
   final bool acceptingMembers;
+
+  String get name => _isArabic ? nameAr : nameEn;
+  String get address => _isArabic ? addressAr : addressEn;
 }
 
 final class MembershipLocationData {
