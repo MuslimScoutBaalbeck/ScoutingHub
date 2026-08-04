c4b477348a87e8ac2a6cac53bf7ead627814c66c
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-08-04 05:28:52 +0300

    Add Dio authentication interceptor

---
+++ lib/core/network/auth_interceptor.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:scouting_hub/core/network/token_storage.dart';
+
+@lazySingleton
+final class AuthInterceptor extends Interceptor {
+  AuthInterceptor(this._tokenStorage);
+
+  final TokenStorage _tokenStorage;
+
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    options.headers.putIfAbsent('Accept', () => 'application/json');
+
+    final token = await _tokenStorage.readAccessToken();
+    if (token != null && token.isNotEmpty) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+
+    handler.next(options);
+  }
+
+  @override
+  Future<void> onError(
+    DioException error,
+    ErrorInterceptorHandler handler,
+  ) async {
+    if (error.response?.statusCode == 401) {
+      await _tokenStorage.clearAccessToken();
+    }
+
+    handler.next(error);
+  }
+}
